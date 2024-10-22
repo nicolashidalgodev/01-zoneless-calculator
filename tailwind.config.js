f420a51a2da8da81be16b4a8a6448e48d96ceaf1
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-10-22 00:28:26 -0300

    fix: tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /./,
+    }
+  ],
 }
 
